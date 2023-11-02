fbf394a3effc1727f8cdf5503fa8dba2575a4ee3
Przemek Niedziela <przemek.niedziela@gmail.com>
2023-11-02 13:46:43 +0100

    action pubslish on npm

--- src/common/Tools.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateId = void 0;
-let lastGeneratedId = Date.now();
-function generateId() {
-    return `w_${lastGeneratedId++}`;
-}
-exports.generateId = generateId;
--- src/tools/Tools.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.escapeRegExp = void 0;
-function escapeRegExp(value) {
-    return value.replace(/[/\-\\^$*+?.()|[\]{}]/g, '\\$&');
-}
-exports.escapeRegExp = escapeRegExp;
